9e5aea1c03e84413b084eed82fec2c018574d61b
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-06-14 17:18:02 +0300

    remove some folders from index

--- back/dist/auth/auth.constants.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const USER_ALREADY_REGISTERED_ERROR = "User already registered";
--- back/dist/auth/auth.constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_ALREADY_REGISTERED_ERROR = void 0;
-exports.USER_ALREADY_REGISTERED_ERROR = "User already registered";
-//# sourceMappingURL=auth.constants.js.map
