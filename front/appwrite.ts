071f9a0dfd9af8ba9005744b75138c82d8227f10
ArtemZayarny <artyom.zayarny@gmail.com>
2025-09-16 17:09:22 +0300

    fix: add setSelfSigned to Appwrite client for CORS issues

--- front/appwrite.ts
+++ front/appwrite.ts
@@ -4,7 +4,8 @@ const client = new Client();
 
 client
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+  .setSelfSigned(true); // Помогает с CORS проблемами
 
 const account = new Account(client);
 const storage = new Storage(client);
