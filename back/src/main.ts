f5ba9d967dd2f569bff659ace724d37395b09861
ArtemZayarny <artyom.zayarny@gmail.com>
2025-12-26 10:21:28 +0200

    Added cors allowed urls

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,7 +3,18 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  
+  const allowedOrigins = [
+    process.env.CLIENT_URL ? process.env.CLIENT_URL : "http://localhost:3000"
+  ];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
+  
   app.setGlobalPrefix("api");
 
   await app.listen(process.env.PORT || 3001);
