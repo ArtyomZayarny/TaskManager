b539f7e3a674e71656ad36342c4584805364cfcc
ArtemZayarny <artyom.zayarny@gmail.com>
2025-12-26 10:09:43 +0200

    fixed base url constant

--- front/constants.ts
+++ front/constants.ts
@@ -1,14 +1,14 @@
-// Определяем окружение
+
 const isProduction = process.env.NODE_ENV === "production";
 
-// Настройки для разных окружений
+
 export const BASE_URL = isProduction
-  ? process.env.API_BASE_URL
+  ? "https://task-manager-seven-coral-21.vercel.app"
   : "http://localhost";
 export const BASE_PORT = isProduction ? "" : "3001";
 export const BASE_PREFIX = "api";
 
-// Формируем API строку в зависимости от окружения
+
 export const BASE_API_STRING = isProduction
   ? `${BASE_URL}/${BASE_PREFIX}`
   : `${BASE_URL}:${BASE_PORT}/${BASE_PREFIX}`;
